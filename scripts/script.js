5278e1055ed09951bef0d9951274254b68f58c14
Matteo Persiani <persianimatteo@gmail.com>
2024-08-22 14:40:52 -0700

    -Major styling changes -Modified class content

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var hamburger = document.querySelector('.hamburger-menu');
-    var nav = document.querySelector('nav');
-  
-    hamburger.addEventListener('click', function () {
-        nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
-    });
-});
